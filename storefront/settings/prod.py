b59dbeda32b63a1dd7779e11f4a2e84d2920515a
hardebite <adedoyinadegbite.s@gmail.com>
2022-09-12 19:32:37 +0100

    add add-ons

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,8 +1,13 @@
 import os
 from .common import *
+import dj_database_url
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adebuy-prod.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
